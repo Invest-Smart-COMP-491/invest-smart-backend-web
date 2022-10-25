2ce23d1ce0346a387aba7ad88008a360590e2cd4
Metehan Gelgi <mgelgi17@ku.edu.tr>
2022-10-25 20:46:33 +0300

    initial commit with django proje

---
+++ investsmart/investsmart/views.py
@@ -0,0 +1,15 @@
+# HttpResponse is used to
+# pass the information
+# back to view
+from django.http import HttpResponse
+
+# Defining a function which
+# will receive request and
+# perform task depending
+# upon function definition
+def hello_world (request) :
+
+	# This will return Hello Geeks
+	# string as HttpResponse
+	return HttpResponse("Hello World")
+
