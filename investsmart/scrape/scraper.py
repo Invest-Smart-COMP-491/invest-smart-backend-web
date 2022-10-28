ee0c86e2690f0c85c9c011b97261e7b2af0c6376
berkecanrizai <berke.rizai@getir.com>
2022-10-28 22:13:11 +0300

    feat: init scraper

---
+++ investsmart/scrape/scraper.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+
+from investsmart.scrape.constants import STOCKS_LIST
+
+
+class LivePrice:
+    def __init__(self, stock_name):
+        self.stock_name = stock_name
+
+    def getLastPrice(self):
+        data = yf.download(tickers=self.stock_name, period='1m', interval='1m')
+        return data['Open'].iloc[-1]
+
+    def getCompanyInfo(self):
+        stock = yf.Ticker(self.stock_name)
+        info_dict = stock.info
+
+        dc = {k: v for k, v in info_dict.items() if k in ['sector', 'shortName', 'logo_url', 'marketCap', 'industry']}
+        return dc
+
+    def getTarget(self):
+        return finviz.get_analyst_price_targets(self.stock_name)
+
+
+class DBUpdater():
+    def __init__(self):
+        pass
+
+    def updateAllLastPrices(self):
+        map(self.updateLastPrice, STOCKS_LIST)
+
+        """for st in STOCKS_LIST:
+            lp = LivePrice(st)
+            last_price = lp.getLastPrice()
+            self.updateLastPrice(st, last_price)"""
+
+    def updateLastPrice(self, ticker):
+        lp = LivePrice(ticker)
+        last_price = lp.getLastPrice()
+        db.update(ticker, last_price)
+
+    def updateAllAnalystTargets(self):
+        map(self.updateAnalystTargets, STOCKS_LIST)
+
+    def updateAnalystTargets(self, ticker):
+        lp = LivePrice(ticker)
+        targets = lp.getLastPrice()
+
+        """{'date': '2022-10-28',
+         'category': 'Reiterated',
+         'analyst': 'Wedbush',
+         'rating': 'Outperform',
+         'target_from': 220.0,
+         'target_to': 200.0}"""
+        for tar in targets:
+            db.update(tar)
