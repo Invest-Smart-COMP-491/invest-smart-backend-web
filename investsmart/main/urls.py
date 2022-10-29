a920eb999a125a5f0226d0ab0bb1597c8412ae8f
Metehan Gelgi <mgelgi17@ku.edu.tr>
2022-10-29 18:55:24 +0300

    register - login using django features

--- investsmart/main/urls.py
+++ investsmart/main/urls.py
@@ -20,5 +20,8 @@ app_name = "main"
 
 urlpatterns = [
 	path("",views.homepage,name="homepage"),
+	path("register/",views.register,name="register"),
+	path("login/",views.login_request,name="login"),
+	path("logout/",views.logout_request,name="logout"),
 
 ]
