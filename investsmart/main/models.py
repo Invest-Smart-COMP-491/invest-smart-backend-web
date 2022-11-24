a122b579b3ce02458ba25bf9945a0b4eacf16653
berkecanrizai <berke.rizai@getir.com>
2022-11-24 23:05:19 +0300

    fix: rm unnecessary

--- investsmart/main/models.py
+++ investsmart/main/models.py
@@ -121,22 +121,3 @@ class CommentLike(models.Model): # TODO: dönerken tüm comment likelarını dö
 
 	class Meta:
 		verbose_name_plural = 'Commentlikes'
-
-
-
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published', null=True)
-    author = models.CharField(max_length=200, null=True)
-
-    def __str__(self):
-        return self.question_text
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
