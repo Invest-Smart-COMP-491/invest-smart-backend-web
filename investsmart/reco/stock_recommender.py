2feb96a3131325c87d9e94dda61169d1ade98fb9
berkecanrizai <berke.rizai@getir.com>
2022-12-01 17:40:50 +0300

    fix: refactor- rm comments

--- investsmart/reco/stock_recommender.py
+++ investsmart/reco/stock_recommender.py
@@ -22,8 +22,7 @@ class Recommender:
         posts2 = reddit.getPosts(subreddit="stocks", lim=500)
 
         posts = pd.concat([posts, posts2])
-        # return posts
-        #print(posts)
+
         for row in posts[['title', 'body']].to_dict():
             stonk = re.findall(r'[A-Z]{3}(?<![A-Z]{4})(?![A-Z])',
                                (str([posts['title'].iloc[i] for i in range(len(posts['title']))])).replace("\\",
@@ -43,7 +42,6 @@ class Recommender:
                 if stock in STOCK_TICKERS_LIST:
                     freq_dict[stock] = freq_dict.get(stock, 0) + 1
 
-        print('dc', freq_dict)
         return freq_dict
 
 class SimilarStocks:
@@ -58,7 +56,6 @@ class SimilarStocks:
     def buildSimilarityDict(self):
         #news = News.objects.all()
         #news = news[['asset', 'mentioned_asset']]
-
         df = pd.DataFrame(list(News.objects.all().values('asset', 'mentioned_asset')))
         reco_dict = {}
         for main_asset, mentioned_asset in df.iterrows():
@@ -67,7 +64,7 @@ class SimilarStocks:
                 reco_dict.get(main_asset, []).append(mentioned_asset)
 
         self.reco_dict = reco_dict
-        print(reco_dict)
+
         #print(News.objects.all().values('asset', 'mentioned_asset'))
 
     def getSimilarAssets(self, asset):
