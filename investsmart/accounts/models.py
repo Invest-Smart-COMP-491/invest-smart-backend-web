39b5e7ca171d2e269f54fef9dc6cb30438dc293e
Metehan Gelgi <mgelgi17@ku.edu.tr>
2022-12-10 15:11:09 +0300

    public_name

--- investsmart/accounts/models.py
+++ investsmart/accounts/models.py
@@ -4,9 +4,16 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-	public_name = models.CharField(max_length = 20,unique=True)
+	#public_name = models.CharField(max_length = 20,unique=True)
+
+	public_name = models.CharField(max_length = 20,default=None, null=True)
     # add additional fields in here
     
 
 	def __str__(self):
 		return self.username
+	
+	def save(self, *args, **kwargs) :
+		if not self.public_name:
+			self.public_name = self.username
+		super(CustomUser, self).save(*args, **kwargs)
